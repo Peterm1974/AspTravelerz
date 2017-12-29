cb0a49361038f603fd12766eadf85dab1f64ea3d
Peter Morgan <petermorgan74@gmail.com>
2017-12-29 16:16:45 +1300

    First.

--- AspTravelerz/Startup.cs
+++ AspTravelerz/Startup.cs
@@ -6,6 +6,9 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.FileProviders;
+using System.IO;
 
 namespace AspTravelerz
 {
@@ -15,20 +18,60 @@ namespace AspTravelerz
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddDirectoryBrowser();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddConsole(LogLevel.Information);
+
+            //app.UseExceptionHandler("/error.html");
+
+            //app.UseDefaultFiles();
+            app.UseDefaultFiles(new DefaultFilesOptions
+            {
+                DefaultFileNames = new List<string> { "index.html", "defaultFile.html" }
+            });
+
+            //app.UseStaticFiles(new StaticFileOptions()
+            //{
+            //    FileProvider = new PhysicalFileProvider(
+            //    Path.Combine(Directory.GetCurrentDirectory(), @"StuffOnDisk")),
+            //    RequestPath = new PathString("/StaticFiles")
+            //});
+
+            //test
+
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider(
+                        Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot", "assets")),
+                RequestPath = new PathString("/assets")
+            });
+
+            app.UseDirectoryBrowser(new DirectoryBrowserOptions()
+            {
+                FileProvider = new PhysicalFileProvider(
+                    Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot", "assets")),
+                RequestPath = new PathString("/assets")
+            });
+
+            app.UseMvc(routes =>
+                routes.MapRoute(
+                  name: "default",
+                  template: "{controller = Home}/{action = Index}/{id ?}"));
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
             app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+                        {
+                            await context.Response.WriteAsync("Hello World!");
+                        });
         }
     }
 }
